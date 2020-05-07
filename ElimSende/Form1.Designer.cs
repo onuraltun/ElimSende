caef2ee98f79417384f164af57aa5945c104fd5d
onur altun <onuraltun@hotmail.com>
2020-05-07 15:32:58 +0300

    Add project files.

---
+++ ElimSende/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ElimSende
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnIptal = new System.Windows.Forms.Button();
+            this.txtLimit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 22);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1116, 22);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = resources.GetString("txtUrl.Text");
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(12, 104);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(90, 36);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnIptal
+            // 
+            this.btnIptal.Location = new System.Drawing.Point(108, 104);
+            this.btnIptal.Name = "btnIptal";
+            this.btnIptal.Size = new System.Drawing.Size(90, 36);
+            this.btnIptal.TabIndex = 2;
+            this.btnIptal.Text = "İptal";
+            this.btnIptal.UseVisualStyleBackColor = true;
+            this.btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+            // 
+            // txtLimit
+            // 
+            this.txtLimit.Location = new System.Drawing.Point(12, 63);
+            this.txtLimit.Name = "txtLimit";
+            this.txtLimit.Size = new System.Drawing.Size(100, 22);
+            this.txtLimit.TabIndex = 3;
+            this.txtLimit.Text = "500000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1285, 369);
+            this.Controls.Add(this.txtLimit);
+            this.Controls.Add(this.btnIptal);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnIptal;
+        private System.Windows.Forms.TextBox txtLimit;
+    }
+}
+
---
+++ ElimSende/Form1.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ElimSende
+{
+    public partial class Form1 : Form
+    {
+        bool trace = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("user-data-dir=C:\\Users\\onur.altun\\AppData\\Local\\Google\\Chrome\\User Data");
+            options.AddArgument("--profile-directory=Default");
+            options.AddArgument("--start-maximized");
+
+            ChromeDriver driver = new ChromeDriver(options);
+
+            driver.Url = txtUrl.Text;
+
+            Thread.Sleep(10000);
+
+            var allCells = driver.FindElements(By.XPath("//react-feed-listener-cell"));
+
+            var bidCells = allCells.Where(c => c.GetAttribute("field-name") == "bidAmount1").ToList();
+
+            int.TryParse(txtLimit.Text, out var limit);
+
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+            var result = wait.Until((webDriver) =>
+            {
+                do
+                {
+                    foreach (var bidCell in bidCells)
+                    {
+                        long.TryParse(bidCell.Text.Replace(".", ""), out long currentBid);
+
+                        if (limit > currentBid && currentBid > 0)
+                        {
+                            return bidCells[0].GetAttribute("security-name");
+                        }
+                    }
+
+                    Thread.Sleep(100);
+                } while (trace);
+
+                return null;
+            });
+
+            if (trace && !string.IsNullOrWhiteSpace(result))
+            {
+                var buttons = driver.FindElements(By.Id("quick_order_sell"));
+
+                foreach (var button in buttons)
+                {
+                    button.Click();
+                }
+            }
+        }
+
+        private void btnIptal_Click(object sender, EventArgs e)
+        {
+            trace = false;
+        }
+    }
+}
